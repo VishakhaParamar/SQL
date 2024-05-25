29a8b2cdaecb8cf46c8ea63ef8cc26211cde79f0
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-25 12:29:57 +0530

    Update Create-table.sql
    creating two table for adding marks.

--- Create-table.sql
+++ Create-table.sql
@@ -1,5 +1,13 @@
+-- create table for btech student.
 create table btech(
              En_no int,
              Name varchar(20),
              Branch varchar(20),
              Rollno int);
+
+-- create table which can connect with student table.
+create table marks(
+                     Name varchar(20),
+                     Branch varchar(20),
+                     Rollno int,
+                     mark int);
