20398f5d69a90b0f6be922790caa8a70bb96d4c1
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-25 12:22:26 +0530

    Create Insert-data.sql
    Inserting data into table with different type of commands.

---
+++ Insert-data.sql
@@ -0,0 +1,14 @@
+-- here we are adding data in table 
+insert into btech values(1001,'kavya','CSE',1),
+                        (1002,'nishant','IT',2),
+                        (1003,'akay','CSE',3),
+                        (1004,'nishtha','ICT',4),
+                        (1005,'chhaya','IT',5),
+                        (1006,'nevil','CSE',6);
+
+-- here we are adding data in columns
+insert into btech (en_no,name) values 
+                                     (1007,'harry'),
+                                     (1008,'karan');
+
+--here we are adding data into one table to another table
