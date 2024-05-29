f9e663db9952430e96dbe968f001323530923ae4
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-29 16:32:08 +0530

    Update Q6.sql

--- Q6.sql
+++ Q6.sql
@@ -3,9 +3,9 @@
 Here's the basic syntax:
 SELECT REPLACE(original_string, old_substring, new_substring) AS replaced_string
 FROM table;
-original_string: This is the column or expression containing the string where you want to perform the replacement.
-old_substring: This is the specific sequence of characters you want to replace.
-new_substring: This is the text you want to insert in place of the old_substring.
-AS replaced_string: This is an optional alias you can assign to the resulting column containing the modified string.*/
+- original_string: This is the column or expression containing the string where you want to perform the replacement.
+- old_substring: This is the specific sequence of characters you want to replace.
+- new_substring: This is the text you want to insert in place of the old_substring.
+- AS replaced_string: This is an optional alias you can assign to the resulting column containing the modified string.*/
 
 select replace(name,'a','A') from btech;
