6bff993f644527b97bbda7933728bf12295757fc
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-29 19:13:34 +0530

    Update Q 20.sql

--- Q 20.sql
+++ Q 20.sql
@@ -1,4 +1,8 @@
 --Write an SQL query to show the second highest mark from a btech table using sub-query.
-
+--1
 select max(mark) from marks
 where mark not in (select max(mark) from marks);
+--2
+select * from marks
+where mark = (select max(mark) from marks
+              where mark <> (select max(mark) from marks));
