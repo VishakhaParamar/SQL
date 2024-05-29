016aec1ee766d28092c3b82cc2f224249bde111a
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-29 13:55:00 +0530

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,3 +1,5 @@
 --Write a SQL query that fetches the unique values of branch from btech table and print its length.
-
+--1
 select distinct branch, length(branch) as size from btech;
+--2
+select branch, length(branch) from btech group by (branch);
