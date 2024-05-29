d1be358542821b68f022939594892b9b226c79fb
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-29 17:31:04 +0530

    Update Insert-data.sql

--- Insert-data.sql
+++ Insert-data.sql
@@ -7,9 +7,9 @@ insert into btech values(1001,'kavya','CSE',1),
                         (1006,'nevil','CSE',6);
 
 -- here we are adding data in columns
-insert into btech (en_no,name) values 
-                                     (1007,'harry'),
-                                     (1008,'karan');
+insert into btech (en_no,name,branch,rollno) values 
+                                     (1007,'harry','CSE',7),
+                                     (1008,'karan','IT',8);
 
 --here we are adding data into one table to another table
 insert into student(En_no,Name, Branch, Rollno)
@@ -17,3 +17,14 @@ select En_no, Name, Branch, Rollno from btech;
 
 insert into marks(Name, Branch, Rollno)
 select Name, Branch, Rollno from student;
+
+--here we are adding specific value in column
+update marks
+set mark = case
+             when rollno = 1 then 98
+             when rollno = 2 then 92
+             when rollno = 4 then 94
+             when rollno = 7 then 93
+             when rollno = 8 then 99
+             else 90
+             end;
