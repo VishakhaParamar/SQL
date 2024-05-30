7fa30e9b90bc7b8b9dc4b645ec3c097e0798abaa
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-30 17:57:25 +0530

    Update Q 21.sql

--- Q 21.sql
+++ Q 21.sql
@@ -1,4 +1,9 @@
---Write an SQL query to determine the nth (say n=4) highest marks from a table marks.
+--Write an SQL query to show the top n (say 4) records of marks table order by descending mark.
 
 select * from marks 
 order by mark desc limit 4;
+
+--Write an SQL query to determine the nth (say n=4) highest mark from a table.
+
+select * from marks 
+order by mark desc limit 4, 1;
