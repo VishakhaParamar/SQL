c61032a466d84ba0467db1f4816a8db6d58938f1
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-31 13:55:38 +0530

    Update Create-table.sql

--- Create-table.sql
+++ Create-table.sql
@@ -1,16 +1,20 @@
 -- create table for btech student.
 create table btech(
              En_no int,
+             Rollno int primary key,
              Name varchar(20),
-             Branch varchar(20),
-             Rollno int);
+             Branch varchar(20)
+             );
 
 -- create table which can connect with student table.
 create table marks(
                      Name varchar(20),
                      Branch varchar(20),
                      Rollno int,
-                     mark int);
+                     mark int,
+                     foreign key (Rollno) references btech(Rollno)
+                     on delete cascade
+                     on update cascade);
 
 -- create table as a existing table.
 create table student as
@@ -20,10 +24,18 @@ select En_no, Name, Branch, Rollno from btech;
 create table branch(
                     En_no int,
                     Branch varchar,
-                    Rollno int);
+                    Rollno int,
+                    foreign key (Rollno) references btech(Rollno)
+                    on delete cascade
+                    on update cascade);
 
 --create table for adding date and for some other functionalities.
 create table ss(
                 Rollno int,
                 ssamount int,
-                ssdate date);
+                ssdate date
+                foreign key (Rollno) references btech(Rollno)
+                on delete cascade
+                on update cascade);
+                
+           
