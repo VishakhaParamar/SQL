850d83a6c1e6e59256bdc96cd8a9f962e7e9081d
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-31 16:21:07 +0530

    Update Q 29.sql

--- Q 29.sql
+++ Q 29.sql
@@ -2,3 +2,8 @@
 
 select * from branch
 where rollno = (select max(rollno) from btech);
+
+--Write an SQL query to fetch the first row of a table.
+
+select * from btech
+where rollno = (select min(rollno) from btech);
