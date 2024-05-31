e50bbed7916b603ca493e469882f8cbd719432d6
Vishakha Parmar <149483670+VishakhaMP@users.noreply.github.com>
2024-05-31 16:25:09 +0530

    Update Q 30.sql

--- Q 30.sql
+++ Q 30.sql
@@ -1,1 +1,6 @@
+--Write an SQL query to fetch the last five records from a table.
 
+select * from (select * from btech 
+               order by rollno desc
+               limit 4) as newaddmision
+               order by rollno;
